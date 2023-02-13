d857525f9b5e0143dc0ed8c17febe7358228c250
Alex <alexsandrolima991@hotmail.com>
2023-02-13 18:23:33 -0300

    (TDD): Endpoint: /cars & tests

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+// import ICar from '../Interfaces/ICar';
+import CarServices from '../Services/CarServices';
+
+export default class CarController {
+  constructor(private _service = new CarServices()) {}
+
+  public async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const newCar = await this._service.create(req.body);
+      return res.status(201).json(newCar);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ tests/unit/Services/CarService.test.ts
@@ -0,0 +1,46 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import CarServices from '../../../src/Services/CarServices';
+import ICar from '../../../src/Interfaces/ICar';
+import Car from '../../../src/Domains/Car';
+
+describe('Deve cadastrar um carro', function () {
+  it('Deve cadastrar um carro com SUCESSO', async function () {
+    // arr
+
+    const newCarInput: ICar = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const returnedCar: ICar = {
+      id: '6348513f34c397abcad040b2',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const newCarOutput: Car = new Car(returnedCar);
+
+    sinon.stub(Model, 'create').resolves(newCarOutput);
+
+    // act
+    
+    const service = new CarServices();
+    const result = await service.create(newCarInput);
+
+    // ass
+
+    expect(result).to.be.deep.equal(newCarOutput);
+  });
+});
