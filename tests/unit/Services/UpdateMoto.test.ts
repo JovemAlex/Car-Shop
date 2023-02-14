f5e295c3803c86c7d120e9cf2d31cff6a6645c6e
Alex <alexsandrolima991@hotmail.com>
2023-02-14 18:02:28 -0300

    (Tests): Implementa os testes de cobertura do endpoint: put /motorcyles/:id

--- tests/unit/Services/UpdateMoto.test.ts
+++ tests/unit/Services/UpdateMoto.test.ts
@@ -1,43 +1,41 @@
 import sinon from 'sinon';
 import { expect } from 'chai';
 import { Model } from 'mongoose';
-import ICar from '../../../src/Interfaces/ICar';
-import CarServices from '../../../src/Services/CarServices';
+import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+import MotorcycleServices from '../../../src/Services/MotorcycleServices';
 
 describe('Atualizar um carro', function () {
   it('Deve atualizar um carro com SUCESSO', async function () {
     // arr
     const id = '634852326b35b59438fbea2f';
 
-    const updateInput: ICar = {
-      model: 'Marea',
-      year: 1992,
+    const updateInput: IMotorcycle = {
+      model: 'Honda Cb 600f Hornet',
+      year: 2014,
       color: 'Red',
       status: true,
-      buyValue: 12.000,
-      doorsQty: 2,
-      seatsQty: 5,
+      buyValue: 45.000,
+      category: 'Street',
+      engineCapacity: 600,
     };
 
-    const updateOutput: ICar = {
+    const updateOutput: IMotorcycle = {
       id: '634852326b35b59438fbea2f',
-      model: 'Marea',
-      year: 1992,
+      model: 'Honda Cb 600f Hornet',
+      year: 2014,
       color: 'Red',
       status: true,
-      buyValue: 12.000,
-      doorsQty: 2,
-      seatsQty: 5,
+      buyValue: 45.000,
+      category: 'Street',
+      engineCapacity: 600,
     };
 
     sinon.stub(Model, 'findByIdAndUpdate').resolves(updateOutput);
 
     // act
-    const service = new CarServices();
+    const service = new MotorcycleServices();
     const result = await service.update(id, updateInput);
     // ass
     expect(result).to.be.deep.equal(updateOutput);
-
-    sinon.restore();
   });
 });
