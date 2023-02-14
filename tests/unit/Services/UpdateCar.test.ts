ab7ffd46b3b018a5bcf5a8204777d0ccb063b67b
Alex <alexsandrolima991@hotmail.com>
2023-02-14 14:49:40 -0300

    (Test): Testes da de cobertura do endpoint: put: /cars/:id

--- tests/unit/Services/UpdateCar.test.ts
+++ tests/unit/Services/UpdateCar.test.ts
@@ -1,41 +1,42 @@
-// import sinon from 'sinon';
-// import { expect } from 'chai';
-// import { Model } from 'mongoose';
-// import ICar from '../../../src/Interfaces/ICar';
-// import Car from '../../../src/Domains/Car';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import ICar from '../../../src/Interfaces/ICar';
+import Car from '../../../src/Domains/Car';
+import CarServices from '../../../src/Services/CarServices';
 
-// describe('Atualizar um carro', function () {
-//   it('Deve atualizar um carro com SUCESSO', async function () {
-//     // arr
+describe('Atualizar um carro', function () {
+  it('Deve atualizar um carro com SUCESSO', async function () {
+    // arr
+    const id = '634852326b35b59438fbea2f';
 
-//     const updateCarInput: ICar = {
-//       model: 'Marea',
-//       year: 1992,
-//       color: 'Red',
-//       status: true,
-//       buyValue: 12.000,
-//       doorsQty: 2,
-//       seatsQty: 5,
-//     };
+    const updateInput = {
+      model: 'Marea',
+      year: 1992,
+      color: 'Red',
+      status: true,
+      buyValue: 12.000,
+      doorsQty: 2,
+      seatsQty: 5,
+    };
 
-//     const updateResult: ICar[] = [
-//       {
-//         id: '634852326b35b59438fbea2f',
-//         model: 'Marea',
-//         year: 2002,
-//         color: 'Black',
-//         status: true,
-//         buyValue: 12.000,
-//         doorsQty: 4,
-//         seatsQty: 5,
-//       }];
+    const updateOutput = {
+      id: '634852326b35b59438fbea2f',
+      model: 'Marea',
+      year: 1992,
+      color: 'Red',
+      status: true,
+      buyValue: 12.000,
+      doorsQty: 2,
+      seatsQty: 5,
+    };
 
-//     const carUpdated: Car[] = updateResult.map((e) => new Car(e));
+    sinon.stub(Model, 'findByIdAndUpdate').resolves(updateOutput);
 
-//     sinon.stub(Model, 'update').resolves(carUpdated);
-
-//     // act
-
-//     // ass
-//   });
-// });
+    // act
+    const service = new CarServices();
+    const result = await service.update(id, updateInput);
+    // ass
+    expect(result).to.be.deep.equal(updateOutput);
+  });
+});
