ee21655fbdff6e116fe8482380d10d8200471480
Alex <alexsandrolima991@hotmail.com>
2023-02-14 16:29:42 -0300

    Refatoração do Domain: Car. Criação do Domain Motorcycle e Vehicle

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,30 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import CategoryOptions from '../utils/CategoryOptions';
+import Vehicle from './Vehicle';
+
+export default class Motorcycle extends Vehicle {
+  private category: CategoryOptions;
+  private engineCapacity: number;
+
+  constructor(motorcycle: IMotorcycle) {
+    super(motorcycle);
+    this.category = motorcycle.category;
+    this.engineCapacity = motorcycle.engineCapacity;
+  }
+
+  public setCategory(category: CategoryOptions) {
+    this.category = category;
+  }
+
+  public getCategory() {
+    return this.category;
+  }
+
+  public setEngineCapacity(engineCapacity: number) {
+    this.engineCapacity = engineCapacity;
+  }
+
+  public getEngineCapacity() {
+    return this.engineCapacity;
+  }
+}
